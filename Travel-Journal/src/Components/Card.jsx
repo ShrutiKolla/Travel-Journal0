aa9437a6eb3038664a4e572b46cad2407a3347c8
me-mystic <Shrutikolla.2002@gmail.com>
2023-07-11 18:39:35 +0530

    .

--- Travel-Journal/src/Components/Card.jsx
+++ Travel-Journal/src/Components/Card.jsx
@@ -8,19 +8,22 @@ import img1 from '../assets/imgs/img1.svg';
 import loc from '../assets/imgs/location.svg';
 export default function Card(prop) {
     return (
-        <card>
-            <img src={prop.img} alt="" className={cardCss.image}/>
-            <div className={cardCss.info}>
-                <div className={cardCss.location}>
-                    <img src={loc} alt="" />
-                    <p>{prop.location}</p>
-                    <a href={prop.url} target='_blank' rel="noreferrer">View on Google Maps</a>
+        <div className="cardDiv">
+            <card>
+                <img src={prop.img} alt="" className={cardCss.image} />
+                <div className={cardCss.info}>
+                    <div className={cardCss.location}>
+                        <img src={loc} alt="" />
+                        <p>{prop.location}</p>
+                        <a href={prop.url} target='_blank' rel="noreferrer">View on Google Maps</a>
+                    </div>
+                    <h1 className={cardCss.name}>{prop.title}</h1>
+                    <p className={cardCss.date}>{prop.startDt} - {prop.endDt}</p>
+                    <p className={cardCss.desc}>{prop.desc}</p>
                 </div>
-                <h1 className={cardCss.name}>{prop.title}</h1>
-                <p className={cardCss.date}>12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className={cardCss.desc}>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
-            </div>
-            <hr />
-        </card>
+            </card>
+            <hr className={cardCss.hr}/>
+
+        </div>
     );
 }
--- Travel-Journal/src/Components/cardsDb.js
+++ Travel-Journal/src/Components/cardsDb.js
@@ -12,7 +12,7 @@ export const cardsArr =
     {
         title : "Sydney Opera House",
         location : "AUSTRALIA",
-        url : "https://goo.gl/maps/uCFsKuHWy469bsoU8",
+        url : "https://goo.gl/maps/ZSezqnxZWpUeb1MaA",
         startDt : "27 May, 2021",
         endDt : "8 Jun, 2021",
         desc : "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
@@ -26,6 +26,15 @@ export const cardsArr =
         endDt : "18 Nov, 2021",
         desc : "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         img : "https://source.unsplash.com/3PeSjpLVtLg"
+    },
+    {
+        title : "The Louvre",
+        location : "PARIS",
+        url : "https://goo.gl/maps/sm54gB7zuJiQddcM9",
+        startDt : "08 Mar, 2021",
+        endDt : "22 Mar, 2021",
+        desc : "It is home to some of the most famous artworks in the world, such as Leonardo da Vinci's Mona Lisa and Virgin of the Rocks. The museum also has a vast collection of sculptures (including Venus de Milo) and other objects from ancient civilizations.",
+        img : "https://images.unsplash.com/photo-1584983818902-ef5a7887c6cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
     }
 ]
 /*Photos from Unsplash
