ed15667df3730efdcce45f347eee059b898211b6
me-mystic <Shrutikolla.2002@gmail.com>
2023-07-11 12:53:39 +0530

    .

---
+++ Travel-Journal/src/Components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// css
+import navCss from '../assets/css/nav.module.css';
+
+// imgs
+import globe from '../assets/imgs/globe.svg';
+export default function Nav() {
+    return(
+        <nav className={navCss.nav}>
+        </nav>
+    );
+}
